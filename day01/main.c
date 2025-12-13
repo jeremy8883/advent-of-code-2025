a6b1259f19f0632c8139c412c6979b388a0ec76a
Jeremy Casey <jeremy8883@gmail.com>
2025-12-13 21:21:35 +1100

    Day 1, Part 1

--- day01/main.c
+++ day01/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int wrap(int n, int mod) {
+    return ((n % mod) + mod) % mod;
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    FILE *f = fopen("day01/input.txt", "r");
+    if (!f) {
+        perror("Failed to open input.txt");
+        return EXIT_FAILURE;
+    }
+
+    int pos = 50;
+    int password = 0;
+
+    char line[32];
+    while (fgets(line, sizeof(line), f)) {
+        char dir;
+        int amount;
+
+        sscanf(line, "%c%d", &dir, &amount);
+
+        if (dir == 'L') {
+            pos = wrap(pos - amount, 100);
+        } else if (dir == 'R') {
+            pos = wrap(pos + amount, 100);
+        }
+        printf("%c%i = %i\n", dir, amount, pos);
+
+        if (pos == 0) {
+            password += 1;
+        }
+    }
+
+    printf("%d\n", password);
+
     return 0;
 }
