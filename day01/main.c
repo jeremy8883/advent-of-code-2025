e1f9824c025bdcbf0421938206b119209237ba9b
Jeremy Casey <jeremy8883@gmail.com>
2025-12-13 22:06:55 +1100

    Day 1, Part 2

--- day01/main.c
+++ day01/main.c
@@ -6,6 +6,11 @@ typedef struct {
     int amount;
 } Instruction;
 
+enum {
+    WRAP_POS = 100,
+    START_POS = 50,
+};
+
 Instruction *read_instructions(size_t *count) {
     FILE *f = stdin;
     if (!f) return NULL;
@@ -38,21 +43,47 @@ int wrap(int n, int mod) {
 }
 
 void part1(Instruction *instructions, size_t count) {
-    int pos = 50;
+    int pos = START_POS;
     int password = 0;
 
     for (size_t i = 0; i < count; i++) {
         Instruction m = instructions[i];
 
         if (m.dir == 'L') {
-            pos = wrap(pos - m.amount, 100);
+            pos = wrap(pos - m.amount, WRAP_POS);
         } else if (m.dir == 'R') {
-            pos = wrap(pos + m.amount, 100);
+            pos = wrap(pos + m.amount, WRAP_POS);
         }
 
         if (pos == 0) {
             password += 1;
         }
+    }
+
+    printf("%d\n", password);
+}
+
+void part2(Instruction *instructions, size_t count) {
+    int pos = START_POS;
+    int password = 0;
+
+    for (size_t i = 0; i < count; i++) {
+        Instruction m = instructions[i];
+        int new_pos;
+
+        if (m.dir == 'L') {
+            new_pos = pos - m.amount;
+        } else if (m.dir == 'R') {
+            new_pos = pos + m.amount;
+        }
+        if (new_pos < 0) {
+            password += (pos == 0 ? 0 : 1) + (-new_pos / WRAP_POS);
+        } else if (new_pos > 0) {
+            password += new_pos / WRAP_POS;
+        } else { // is zero
+            password += 1;
+        }
+        pos = wrap(new_pos, WRAP_POS);
     }
 
     printf("%d\n", password);
@@ -64,6 +95,7 @@ int main(void) {
     if (!instructions) return EXIT_FAILURE;
 
     part1(instructions, count);
+    part2(instructions, count);
 
     return 0;
 }
