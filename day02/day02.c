b77df8019220167f1ad978a293f7b2340d4bd3dc
Jeremy Casey <jeremy8883@gmail.com>
2025-12-14 12:45:17 +1100

    Day 2, Part 2

--- day02/day02.c
+++ day02/day02.c
@@ -62,6 +62,47 @@ long part1(Range *ranges, size_t count) {
     return invalid_sum;
 }
 
+bool is_invalid_2(long val) {
+    char str_val[32];
+    sprintf(str_val, "%ld", val);
+    size_t total_len = strlen(str_val);
+
+    int mid = total_len / 2;
+
+    for (int pattern_len = 1; pattern_len <= mid; pattern_len++) {
+        if (total_len % pattern_len != 0) {
+            continue;
+        }
+
+        char* pattern = strndup(str_val, pattern_len);
+        bool all_match = true;
+        for (int i = pattern_len; i < total_len; i += pattern_len) {
+            char* sub_val = strndup(str_val + i, pattern_len);
+
+            if (strcmp(pattern, sub_val) != 0) {
+                all_match = false;
+                break;
+            }
+        }
+
+        if (all_match) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 long part2(Range *ranges, size_t count) {
-    return 0;
+    long invalid_sum = 0;
+    for (size_t i = 0; i < count; i++) {
+        Range r = ranges[i];
+
+        for (size_t i = r.start; i <= r.end; i++) {
+            if (is_invalid_2(i)) {
+                invalid_sum += i;
+            }
+        }
+    }
+    return invalid_sum;
 }
--- day02/day02_test.c
+++ day02/day02_test.c
@@ -8,7 +8,7 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_part1_simple(void) {
+void test_part1(void) {
     const char *input =
         "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
@@ -22,8 +22,23 @@ void test_part1_simple(void) {
     TEST_ASSERT_EQUAL(1227775554, result);
 }
 
+void test_part2(void) {
+    const char *input =
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+    FILE *f = fmemopen((void *)input, strlen(input), "r");
+    size_t count;
+    Range *ranges = read_input(f, &count);
+    fclose(f);
+
+    long result = part2(ranges, count);
+
+    TEST_ASSERT_EQUAL(4174379265, result);
+}
+
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_part1_simple);
+    RUN_TEST(test_part1);
+    RUN_TEST(test_part2);
     return UNITY_END();
 }
